7e029dc42993149f25660734b3525974573f7f6a
baiheyufei <BaiHeYuFei@outlook.com>
2025-05-19 20:33:43 +0800

    Initial Commit
    With an `ApplicationAdapter` project set up by gdx-liftoff.

---
+++ core/src/main/java/com/badlogic/yatdp/Main.java
@@ -0,0 +1,7 @@
+package com.badlogic.yatdp;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+}
